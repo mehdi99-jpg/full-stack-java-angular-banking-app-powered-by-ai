e89c0d192ebbbc084df0e9999816b348d3b3c8ac
Mehdi <hyndimehdi@gmail.com>
2026-07-06 15:07:57 +0100

    initial commit, setting up dependecies for the project

---
+++ src/test/java/ma/enset/ebankingapp/EBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.ebankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
