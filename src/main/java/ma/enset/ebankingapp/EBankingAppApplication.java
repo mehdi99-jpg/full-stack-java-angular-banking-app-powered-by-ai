b8d4e4f00ba4a8449c92d9ad6595442f7b06f156
Mehdi <hyndimehdi@gmail.com>
2026-07-06 17:37:05 +0100

    mocking objects to test their injection to my h2 database

--- src/main/java/ma/enset/ebankingapp/EBankingAppApplication.java
+++ src/main/java/ma/enset/ebankingapp/EBankingAppApplication.java
@@ -1,7 +1,15 @@
 package ma.enset.ebankingapp;
 
+import ma.enset.ebankingapp.entities.Customer;
+import ma.enset.ebankingapp.repositories.AccountOperationRepository;
+import ma.enset.ebankingapp.repositories.BankAccountRepository;
+import ma.enset.ebankingapp.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EBankingAppApplication {
@@ -10,4 +18,16 @@ public class EBankingAppApplication {
         SpringApplication.run(EBankingAppApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository, BankAccountRepository bankAccountRepository
+    , AccountOperationRepository accountOperationRepository){
+        return args -> {
+            Stream.of("Mehdi","Hassan","Khadija","Wissal","Hamza").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                customerRepository.save(customer);
+            });
+        };
+    }
 }
